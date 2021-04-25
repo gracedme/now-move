b9994eb95037a7ee04b49faf927517cc63c1c41b
Glitch (solar-river-machine) <none>
2021-04-25 01:20:30 +0000

    🏰💪 Updated with Glitch - more types of prompts + colours

--- app/components/Prompts.jsx
+++ app/components/Prompts.jsx
@@ -15,6 +15,69 @@ const grooves = [
   'cabbage patch'
 ];
 
+const shapes = [
+  '_',
+  '|',
+  '+',
+  'X',
+  '=',
+  '~',
+  'O',
+  'T',
+  'Y',
+  'U',
+  '/\\',
+  'V'
+];
+
+const patterns = [
+  'one, two, threeeeee',
+  'one, two, both',
+  'single, single, double',
+  'boom, boom, kha',
+  'up, up, up, down'
+];
+
+const body = [
+  'arm',
+  'leg',
+  'hand',
+  'chest',
+  'hips',
+  'knees',
+  'neck',
+  'elbows',
+  'feet',
+  'head'
+]
+
+const colours = [
+  'BlueViolet',
+  'DarkMagenta',
+  'DarkTurquoise',
+  'DarkViolet',
+  'DeepPink',
+  'DodgerBlue',
+  'HotPink',
+  'Indigo',
+  'MediumOrchid',
+  'MediumPurple',
+  'MediumSeaGreen',
+  'MediumSlateBlue',
+  'MediumTurquoise',
+  'MediumVioletRed',
+  'Rebeccapurple',
+  'RoyalBlue',
+  'SlateBlue'
+]
+
+const combos = [
+  [body, patterns],
+  [body, shapes],
+  [shapes, patterns],
+  [grooves, body]
+]
+
 const getRandomNumber = (min, max) => {
   min = Math.ceil(min);
   max = Math.floor(max);
@@ -22,29 +85,31 @@ const getRandomNumber = (min, max) => {
 }
 
 /* the main page for the index route of this app */
-const HelloWorld = function() {
-  const [count, setCount] = React.useState(0);
-  const [prompt, setPrompt] = React.useState(grooves[getRandomNumber(0, grooves.length - 1)]);
+const Prompts = function() {
+  const [combo, setCombo] = React.useState(combos[getRandomNumber(0, combos.length - 1)])
+  const [comboOne, setComboOne] = React.useState(combo[0]);
+  const [comboTwo, setComboTwo] = React.useState(combo[1]);
+  const [colour, setColour] = React.useState(colours[getRandomNumber(0, colours.length - 1)])
   
   React.useEffect(() => {
     const timeout = setTimeout(() => {
-      setCount(getRandomNumber(0, grooves.length - 1));
-      setPrompt(grooves[count])
-    }, 60000);
+      setComboOne(comboOne[getRandomNumber(0, comboOne.length - 1)]);
+      setComboTwo(comboTwo[getRandomNumber(0, comboTwo.length - 1)]);
+      setColour(colours[getRandomNumber(0, colours.length - 1)])
+    }, 5000);
     
     return () => clearTimeout(timeout);
-  }, [count]);
+  }, [comboOne, comboTwo, colour]);
   
   return (
     <div>
-      <p>don't think</p>
-      
-      <p>just move</p>
-      
-      <h1>{prompt}</h1>
+
+      <h1 style={{color: colour, fontFamily: 'Arial', fontSize: 100}}>{comboOne}</h1>
+      <br />
+      <h1 style={{color: colour, fontFamily: 'Arial', fontSize: 100}}>{comboTwo}</h1>
       
     </div>
   );
 }
 
-module.exports = HelloWorld;
+module.exports = Prompts;
