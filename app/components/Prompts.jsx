0aabbce914a8702a5a3cc8ef1ff23e7413f7508b
Glitch (solar-river-machine) <none>
2021-04-26 02:38:45 +0000

    🐴🔑 Updated with Glitch - sketching out prompt selection

--- app/components/Prompts.jsx
+++ app/components/Prompts.jsx
@@ -1,56 +1,5 @@
 const React = require('react');
 
-const grooves = [
-  'roger rabbit',
-  'steve martin',
-  'bart simpson',
-  'reebok',
-  'monestary',
-  'smurf',
-  'reject',
-  'shamrock',
-  'charleston',
-  'biz markie',
-  'ATL stomp',
-  'cabbage patch'
-];
-
-const shapes = [
-  '_',
-  '|',
-  '+',
-  'X',
-  '=',
-  '~',
-  'O',
-  'T',
-  'Y',
-  'U',
-  '/\\',
-  'V'
-];
-
-const patterns = [
-  'one, two, threeeeee',
-  'one, two, both',
-  'single, single, double',
-  'boom, boom, kha',
-  'up, up, up, down'
-];
-
-const body = [
-  'arm',
-  'leg',
-  'hand',
-  'chest',
-  'hips',
-  'knees',
-  'neck',
-  'elbows',
-  'feet',
-  'head'
-]
-
 const colours = [
   'BlueViolet',
   'DarkMagenta',
@@ -71,12 +20,56 @@ const colours = [
   'SlateBlue'
 ]
 
-const combos = [
-  [body, patterns],
-  [body, shapes],
-  [shapes, patterns],
-  [grooves, body]
-]
+const mix = {
+  body: [
+    'arm',
+    'leg',
+    'hand',
+    'chest',
+    'hips',
+    'knees',
+    'neck',
+    'elbows',
+    'feet',
+    'head'
+  ],
+  shapes: [
+    '_',
+    '|',
+    '+',
+    'X',
+    '=',
+    '~',
+    'O',
+    'T',
+    'Y',
+    'U',
+    '/\\',
+    'V'
+  ],
+  patterns: [
+    'one! two! threeeeee',
+    'one, two, both',
+    'single, single, double',
+    'boom, boom, kha',
+    'up, up, up, down'
+  ],
+  grooves: [
+    'roger rabbit',
+    'steve martin',
+    'bart simpson',
+    'reebok',
+    'monestary',
+    'smurf',
+    'reject',
+    'shamrock',
+    'charleston',
+    'biz markie',
+    'ATL stomp',
+    'cabbage patch'
+  ],
+};
+
 
 const getRandomNumber = (min, max) => {
   min = Math.ceil(min);
@@ -86,28 +79,49 @@ const getRandomNumber = (min, max) => {
 
 /* the main page for the index route of this app */
 const Prompts = function() {
-  const [combo, setCombo] = React.useState(combos[getRandomNumber(0, combos.length - 1)])
-  const [comboOne, setComboOne] = React.useState(combo[0]);
-  const [comboTwo, setComboTwo] = React.useState(combo[1]);
-  const [colour, setColour] = React.useState(colours[getRandomNumber(0, colours.length - 1)])
+  const [selectedMix, setSelectedMix] = React.useState({body: true, shapes: true, patterns: true, grooves: true});
+  const [prompts, setPrompts] = React.useState([]);
+  const [colour, setColour] = React.useState(colours[getRandomNumber(0, colours.length - 1)]);
+  
+  const handleSingleCheck = (e) => {
+    setSelectedMix({
+      ...selectedMix,
+      [e.target.name]: e.target.checked
+    })
+  };
   
   React.useEffect(() => {
     const timeout = setTimeout(() => {
-      setComboOne(comboOne[getRandomNumber(0, comboOne.length - 1)]);
-      setComboTwo(comboTwo[getRandomNumber(0, comboTwo.length - 1)]);
-      setColour(colours[getRandomNumber(0, colours.length - 1)])
-    }, 5000);
+      setColour(colours[getRandomNumber(0, colours.length - 1)]);
+      
+      const newPrompts = Object.keys(selectedMix)
+        .filter(k => selectedMix[k] === true)
+        .map(m => mix[m][getRandomNumber(0, mix[m].length - 1)]);
+      setPrompts(newPrompts);
+      
+    }, 30000);
     
     return () => clearTimeout(timeout);
-  }, [comboOne, comboTwo, colour]);
+  }, [colour, prompts]);
   
   return (
     <div>
 
-      <h1 style={{color: colour, fontFamily: 'Arial', fontSize: 100}}>{comboOne}</h1>
-      <br />
-      <h1 style={{color: colour, fontFamily: 'Arial', fontSize: 100}}>{comboTwo}</h1>
+      <h2>Select from:</h2>
+        {Object.keys(mix).map(k => {
+          return (
+            <div>
+              <input type="checkbox" 
+                name={k} 
+                checked={selectedMix[k]} 
+                onChange={handleSingleCheck} 
+              />
+              {k}
+            </div>
+          )})}
       
+      <h2>Prompts:</h2>
+      {prompts.map(p => <h1 style={{color: colour, fontFamily: 'Arial', fontSize: 100}}>{p}</h1>)}
     </div>
   );
 }
